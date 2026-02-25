911d063664a4924425c05cfedd4aaca82c7f9e85
AZ-IDD <amr@idd.co.il>
2026-02-25 09:09:01 -0500

    Revert "Fix missing email id and add Phone field"
    This reverts commit ca6cae3285ec8d30f9e7c57a70e8795dc8ce607c.

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ document.getElementById("contactForm").addEventListener("submit", function (e) {
 
     const fullName = document.getElementById("fullName").value.trim();
     const email = document.getElementById("email").value.trim();
-    const phone = document.getElementById("phone").value.trim();
 
     const results = document.getElementById("results");
     results.innerHTML =
         "<p><strong>Full Name:</strong> " + fullName + "</p>" +
-        "<p><strong>Email:</strong> " + email + "</p>" +
-        "<p><strong>Phone:</strong> " + phone + "</p>";
+        "<p><strong>Email:</strong> " + email + "</p>";
     results.classList.remove("hidden");
 });
